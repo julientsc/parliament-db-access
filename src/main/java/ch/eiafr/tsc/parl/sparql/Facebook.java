ea2ba91f70c38d54861c80a3dce72ec7d6f696ba
Julien Tscherrig <julien@tscherrig.com>
2015-05-27 00:01:26 +0200

    Prepare SPARQL structure for Facebook and Twitter

---
+++ src/main/java/ch/eiafr/tsc/parl/sparql/Facebook.java
@@ -0,0 +1,27 @@
+package ch.eiafr.tsc.parl.sparql;
+
+/**
+ * Created by Julien on 26.05.15.
+ */
+public class Facebook {
+
+    public static String getParlFacebookAccountList() {
+        String query = "";
+        return query;
+    }
+
+    public static String getAccount(long id) {
+        String query = "";
+        return query;
+    }
+
+    public static String getStatusListFromAccount(long id, long startDate, long stopDate) {
+        String query = "";
+        return query;
+    }
+
+    public static String getStatus(long id) {
+        String query = "";
+        return query;
+    }
+}
---
+++ src/main/java/ch/eiafr/tsc/parl/sparql/Twitter.java
@@ -0,0 +1,38 @@
+package ch.eiafr.tsc.parl.sparql;
+
+/**
+ * Created by Julien on 26.05.15.
+ */
+public class Twitter {
+
+    public static String getParlTwitterAccountList() {
+        String query = "";
+        return query;
+    }
+
+    public static String getFriends(long id) {
+        String query = "";
+        return query;
+    }
+
+    public static String getFollower(long id) {
+        String query = "";
+        return query;
+    }
+
+    public static String getAccount(long id) {
+        String query = "";
+        return query;
+    }
+
+    public static String getTweetsListFromAccount(long id, long startDate, long stopDate) {
+        String query = "";
+        return query;
+    }
+
+    public static String getTweet(long id) {
+        String query = "";
+        return query;
+    }
+
+}
