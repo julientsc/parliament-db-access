962cf62136f50ffe071edf770f89e930db4052f4
Julien Tscherrig <julien@tscherrig.com>
2015-05-26 23:02:40 +0200

    Working version ?

--- src/main/java/ch/eiafr/tsc/parl/sparql/Parl.java
+++ src/main/java/ch/eiafr/tsc/parl/sparql/Parl.java
@@ -16,11 +16,10 @@ public class Parl {
 
     public static String getCanton(int id) {
         String strId = "\"" + id + "\"^^<http://www.w3.org/2001/XMLSchema#int>";
-        String queryCantonFilter = "SELECT DISTINCT ?name ?abbr ?item WHERE {\n" +
-                "?item <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Canton> .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasName> ?name .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasAbbr> ?abbr .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasId> " + strId +
+        String queryCantonFilter = "SELECT DISTINCT * WHERE {\n" +
+                "?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Canton> .\n" +
+                "?s <http://eia-fr-ontology.ch/predicate/hasId> " + strId + ".\n" +
+                "?s ?p ?o\n" +
                 "}";
         return queryCantonFilter;
     }
@@ -37,11 +36,10 @@ public class Parl {
 
     public static String getCouncil(int id) {
         String strId = "\"" + id + "\"^^<http://www.w3.org/2001/XMLSchema#int>";
-        String queryCouncilFilter = "SELECT DISTINCT ?name ?abbr ?item WHERE {\n" +
-                "?item <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Councils> .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasName> ?name .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasAbbr> ?abbr .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasId> " + strId +
+        String queryCouncilFilter = "SELECT DISTINCT * WHERE {\n" +
+                "?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Councils> .\n" +
+                "?s <http://eia-fr-ontology.ch/predicate/hasId> " + strId + ".\n" +
+                "?s ?p ?o\n" +
                 "}";
         return queryCouncilFilter;
     }
@@ -58,11 +56,10 @@ public class Parl {
 
     public static String getFaction(int id) {
         String strId = "\"" + id + "\"^^<http://www.w3.org/2001/XMLSchema#int>";
-        String queryFactionFilter = "SELECT DISTINCT ?name ?abbr ?item WHERE {\n" +
-                "?item <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Faction> .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasName> ?name .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasAbbr> ?abbr .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasId> " + strId +
+        String queryFactionFilter = "SELECT DISTINCT * WHERE {\n" +
+                "?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Faction> .\n" +
+                "?s <http://eia-fr-ontology.ch/predicate/hasId> " + strId + ".\n" +
+                "?s ?p ?o\n" +
                 "}";
         return queryFactionFilter;
     }
@@ -79,11 +76,10 @@ public class Parl {
 
     public static String getParty(int id) {
         String strId = "\"" + id + "\"^^<http://www.w3.org/2001/XMLSchema#int>";
-        String queryPartyFilter = "SELECT DISTINCT ?name ?abbr ?item WHERE {\n" +
-                "?item <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Party> .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasName> ?name .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasAbbr> ?abbr .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasId> " + strId +
+        String queryPartyFilter = "SELECT DISTINCT * WHERE {\n" +
+                "?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Party> .\n" +
+                "?s <http://eia-fr-ontology.ch/predicate/hasId> " + strId + ".\n" +
+                "?s ?p ?o\n" +
                 "}";
         return queryPartyFilter;
     }
@@ -99,9 +95,10 @@ public class Parl {
 
     public static String getLang(int id) {
         String strId = "\"" + id + "\"^^<http://www.w3.org/2001/XMLSchema#int>";
-        String queryLangFilter = "SELECT DISTINCT ?item WHERE {\n" +
-                "?item <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Lang> .\n" +
-                "?item <http://eia-fr-ontology.ch/predicate/hasId> " + strId +
+        String queryLangFilter = "SELECT DISTINCT * WHERE {\n" +
+                "?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Lang> .\n" +
+                "?s <http://eia-fr-ontology.ch/predicate/hasId> " + strId + ".\n" +
+                "?s ?p ?o\n" +
                 "}";
         return queryLangFilter;
     }
@@ -122,6 +119,7 @@ public class Parl {
     public static String getCouncillor(int id) {
         String strId = "\"" + id + "\"^^<http://www.w3.org/2001/XMLSchema#int>";
         String queryCouncillorFilter = "SELECT DISTINCT * WHERE {\n" +
+                "?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://eia-fr-ontology.ch/subject/Councillor> .\n" +
                 "?s <http://eia-fr-ontology.ch/predicate/hasId> " + strId + ".\n" +
                 "?s ?p ?o\n" +
                 "}";
