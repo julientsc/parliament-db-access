53bb345c4ef2fb311a203aad0060df73666d0120
Julien Tscherrig <julien@tscherrig.com>
2015-05-26 22:25:57 +0200

    Add method to return a full sparql object

--- src/main/java/ch/eiafr/tsc/parl/StoreRDF.java
+++ src/main/java/ch/eiafr/tsc/parl/StoreRDF.java
@@ -16,11 +16,11 @@ import java.util.HashMap;
 public class StoreRDF {
 
     private static HashMap<String, StoreRDF> stores = null;
-    private static String sesameServer = "http://semantic.ilab-research.ch:8080";
+    private static String sesameServer = "http://160.98.60.121:8080/openrdf-sesame/";
 
     private Repository repo = null;
 
-    private static StoreRDF getInstance(String repositoryID) throws RepositoryException {
+    public static StoreRDF getInstance(String repositoryID) throws RepositoryException {
         if(stores == null)
             stores = new HashMap<String, StoreRDF>();
 
@@ -31,26 +31,52 @@ public class StoreRDF {
     }
 
     private StoreRDF(String repositoryID) throws RepositoryException {
-
         repo = new HTTPRepository(sesameServer, repositoryID);
         repo.initialize();
     }
 
-    private String getJsonFromSparql(String sparql) throws RepositoryException, QueryEvaluationException, MalformedQueryException {
-        ArrayList<HashMap<String, String>>  results = new ArrayList<HashMap<String, String>>();
+    public String getJsonFromSparql(String sparql) throws RepositoryException, QueryEvaluationException, MalformedQueryException {
+        ArrayList<HashMap<String, String>> results = new ArrayList<HashMap<String, String>>();
         TupleQuery nameQuery = repo.getConnection().prepareTupleQuery(QueryLanguage.SPARQL, sparql);
         TupleQueryResult nameResult = nameQuery.evaluate();
-
         while (nameResult.hasNext()) {
             BindingSet bindingSet = nameResult.next();
             HashMap<String, String> line = new HashMap<String, String>();
             for(String name : bindingSet.getBindingNames()) {
-                String value =bindingSet.getValue(name).stringValue();
+                String value = bindingSet.getValue(name).stringValue();
                 line.put(name, value);
             }
             results.add(line);
         }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(results);
+    }
 
+    public String getJsonFromSparqlAllFields(String sparql) throws RepositoryException, QueryEvaluationException, MalformedQueryException {
+        HashMap<String, ArrayList<String>> results = new HashMap<String, ArrayList<String>>();
+        TupleQuery nameQuery = repo.getConnection().prepareTupleQuery(QueryLanguage.SPARQL, sparql);
+        TupleQueryResult nameResult = nameQuery.evaluate();
+        while (nameResult.hasNext()) {
+            BindingSet bindingSet = nameResult.next();
+            HashMap<String, String> line = new HashMap<String, String>();
+
+            String p =bindingSet.getValue("p").stringValue();
+            String o =bindingSet.getValue("o").stringValue();
+
+            if(p.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"))
+                continue;
+
+            p = p.substring(p.lastIndexOf("/") + 1);
+
+            if(!results.containsKey(p))
+                results.put(p, new ArrayList<String>());
+
+            if(o.startsWith("http://eia-fr-ontology.ch/"))
+                o = o.substring("http://eia-fr-ontology.ch/".length());
+
+            results.get(p).add(o);
+
+        }
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         return gson.toJson(results);
     }
