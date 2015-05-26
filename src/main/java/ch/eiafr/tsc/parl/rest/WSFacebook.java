dd243513c3bcc2ee47ebfc0c4e659d1d9f9ef5c2
Julien Tscherrig <julien@tscherrig.com>
2015-05-27 00:00:54 +0200

    Implemente WS Facebook and Twitter

--- src/main/java/ch/eiafr/tsc/parl/rest/WSFacebook.java
+++ src/main/java/ch/eiafr/tsc/parl/rest/WSFacebook.java
@@ -1,25 +1,63 @@
 package ch.eiafr.tsc.parl.rest;
 
 import ch.eiafr.tsc.parl.StoreRDF;
+import ch.eiafr.tsc.parl.sparql.Facebook;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.repository.RepositoryException;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by Julien on 26.05.15.
  */
-@Path("/parl/")
+@Path("/facebook/")
 public class WSFacebook {
 
-    StoreRDF store = null;
+    StoreRDF prodParl = null;
 
     public WSFacebook() {
         try {
-            store = StoreRDF.getInstance("prod-facebook");
+            prodParl = StoreRDF.getInstance("prod-facebook");
         } catch (RepositoryException e) {
             System.err.println("Cannot start WSFacebook");
         }
     }
 
+    @GET
+    @Path("/account/{accountId}")
+    @Produces("application/json;charset=utf-8")
+    public Response getAccount(@PathParam("accountId") long accountId) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = prodParl.getJsonFromSparql(Facebook.getAccount(accountId));
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/parl")
+    @Produces("application/json;charset=utf-8")
+    public Response getParlFacebookAccountList() throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = prodParl.getJsonFromSparql(Facebook.getParlFacebookAccountList());
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/status/{statusId}")
+    @Produces("application/json;charset=utf-8")
+    public Response getStatus(@PathParam("statusId") long statusId) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = prodParl.getJsonFromSparql(Facebook.getStatus(statusId));
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/status/{accountId}/{startDate}/{stopDate}")
+    @Produces("application/json;charset=utf-8")
+    public Response getStatusListFromAccount(@PathParam("accountId") long accountId, @PathParam("startDate") long startDate, @PathParam("stopDate") long stopDate) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = prodParl.getJsonFromSparql(Facebook.getStatusListFromAccount(accountId, startDate, stopDate));
+        return Response.status(200).entity(output).build();
+    }
 
 }
--- src/main/java/ch/eiafr/tsc/parl/rest/WSTwitter.java
+++ src/main/java/ch/eiafr/tsc/parl/rest/WSTwitter.java
@@ -1,14 +1,21 @@
 package ch.eiafr.tsc.parl.rest;
 
 import ch.eiafr.tsc.parl.StoreRDF;
+import ch.eiafr.tsc.parl.sparql.Twitter;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.repository.RepositoryException;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by Julien on 26.05.15.
  */
-@Path("/parl/")
+@Path("/twitter/")
 public class WSTwitter {
 
     StoreRDF store = null;
@@ -20,6 +27,55 @@ public class WSTwitter {
             System.err.println("Cannot start Twitter");
         }
     }
+
+    @GET
+    @Path("/account/{accountId}")
+    @Produces("application/json;charset=utf-8")
+    public Response getAccount(@PathParam("accountId") long accountId) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = store.getJsonFromSparql(Twitter.getAccount(accountId));
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/account/{accountId}")
+    @Produces("application/json;charset=utf-8")
+    public Response getFollower(@PathParam("accountId") long accountId) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = store.getJsonFromSparql(Twitter.getFollower(accountId));
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/account/{accountId}")
+    @Produces("application/json;charset=utf-8")
+    public Response getFriends(@PathParam("accountId") long accountId) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = store.getJsonFromSparql(Twitter.getFriends(accountId));
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/parl")
+    @Produces("application/json;charset=utf-8")
+    public Response getParlTwitterAccountList() throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = store.getJsonFromSparql(Twitter.getParlTwitterAccountList());
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/tweet/{tweetId}")
+    @Produces("application/json;charset=utf-8")
+    public Response getTweet(@PathParam("tweetId") long tweetId) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = store.getJsonFromSparql(Twitter.getTweet(tweetId));
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Path("/tweet/{accountId}/{startDate}/{stopDate}")
+    @Produces("application/json;charset=utf-8")
+    public Response getTweetsListFromAccount(@PathParam("accountId") long accountId, @PathParam("startDate") long startDate, @PathParam("stopDate") long stopDate) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        String output = store.getJsonFromSparql(Twitter.getTweetsListFromAccount(accountId, startDate, stopDate));
+        return Response.status(200).entity(output).build();
+    }
+
 
 
 }
