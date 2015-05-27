f539ebdecc8ba687c36c12ca538c23af6c63cc22
Julien Tscherrig <julien@tscherrig.com>
2015-05-28 01:34:13 +0200

    Create a automatic cache

--- src/main/java/ch/eiafr/tsc/parl/StoreRDF.java
+++ src/main/java/ch/eiafr/tsc/parl/StoreRDF.java
@@ -19,66 +19,74 @@ public class StoreRDF {
     private static String sesameServer = "http://160.98.60.121:8080/openrdf-sesame/";
 
     private Repository repo = null;
-
-    public static StoreRDF getInstance(String repositoryID) throws RepositoryException {
-        if(stores == null)
-            stores = new HashMap<String, StoreRDF>();
-
-        if(!stores.containsKey(repositoryID))
-            stores.put(repositoryID, new StoreRDF(repositoryID));
-
-        return stores.get(repositoryID);
-    }
+    private HashMap<String, String> previousQueries = new HashMap<String, String>();
+    private HashMap<String, String> previousQueriesComplex = new HashMap<String, String>();
 
     private StoreRDF(String repositoryID) throws RepositoryException {
         repo = new HTTPRepository(sesameServer, repositoryID);
         repo.initialize();
     }
 
+    public static StoreRDF getInstance(String repositoryID) throws RepositoryException {
+        if (stores == null)
+            stores = new HashMap<String, StoreRDF>();
+
+        if (!stores.containsKey(repositoryID))
+            stores.put(repositoryID, new StoreRDF(repositoryID));
+
+        return stores.get(repositoryID);
+    }
+
     public String getJsonFromSparql(String sparql) throws RepositoryException, QueryEvaluationException, MalformedQueryException {
-        ArrayList<HashMap<String, String>> results = new ArrayList<HashMap<String, String>>();
-        TupleQuery nameQuery = repo.getConnection().prepareTupleQuery(QueryLanguage.SPARQL, sparql);
-        TupleQueryResult nameResult = nameQuery.evaluate();
-        while (nameResult.hasNext()) {
-            BindingSet bindingSet = nameResult.next();
-            HashMap<String, String> line = new HashMap<String, String>();
-            for(String name : bindingSet.getBindingNames()) {
-                String value = bindingSet.getValue(name).stringValue();
-                line.put(name, value);
+        if (!previousQueries.containsKey(sparql)) {
+            ArrayList<HashMap<String, String>> results = new ArrayList<HashMap<String, String>>();
+            TupleQuery nameQuery = repo.getConnection().prepareTupleQuery(QueryLanguage.SPARQL, sparql);
+            TupleQueryResult nameResult = nameQuery.evaluate();
+            while (nameResult.hasNext()) {
+                BindingSet bindingSet = nameResult.next();
+                HashMap<String, String> line = new HashMap<String, String>();
+                for (String name : bindingSet.getBindingNames()) {
+                    String value = bindingSet.getValue(name).stringValue();
+                    line.put(name, value);
+                }
+                results.add(line);
             }
-            results.add(line);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            previousQueries.put(sparql, gson.toJson(results));
         }
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        return gson.toJson(results);
+        return previousQueries.get(sparql);
     }
 
     public String getJsonFromSparqlAllFields(String sparql) throws RepositoryException, QueryEvaluationException, MalformedQueryException {
-        HashMap<String, ArrayList<String>> results = new HashMap<String, ArrayList<String>>();
-        TupleQuery nameQuery = repo.getConnection().prepareTupleQuery(QueryLanguage.SPARQL, sparql);
-        TupleQueryResult nameResult = nameQuery.evaluate();
-        while (nameResult.hasNext()) {
-            BindingSet bindingSet = nameResult.next();
-            HashMap<String, String> line = new HashMap<String, String>();
+        if (!previousQueriesComplex.containsKey(sparql)) {
+            HashMap<String, ArrayList<String>> results = new HashMap<String, ArrayList<String>>();
+            TupleQuery nameQuery = repo.getConnection().prepareTupleQuery(QueryLanguage.SPARQL, sparql);
+            TupleQueryResult nameResult = nameQuery.evaluate();
+            while (nameResult.hasNext()) {
+                BindingSet bindingSet = nameResult.next();
+                HashMap<String, String> line = new HashMap<String, String>();
 
-            String p =bindingSet.getValue("p").stringValue();
-            String o =bindingSet.getValue("o").stringValue();
+                String p = bindingSet.getValue("p").stringValue();
+                String o = bindingSet.getValue("o").stringValue();
 
-            if(p.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"))
-                continue;
+                if (p.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"))
+                    continue;
 
-            p = p.substring(p.lastIndexOf("/") + 1);
+                p = p.substring(p.lastIndexOf("/") + 1);
 
-            if(!results.containsKey(p))
-                results.put(p, new ArrayList<String>());
+                if (!results.containsKey(p))
+                    results.put(p, new ArrayList<String>());
 
-            if(o.startsWith("http://eia-fr-ontology.ch/"))
-                o = o.substring("http://eia-fr-ontology.ch/".length());
+                if (o.startsWith("http://eia-fr-ontology.ch/"))
+                    o = o.substring("http://eia-fr-ontology.ch/".length());
 
-            results.get(p).add(o);
+                results.get(p).add(o);
 
+            }
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            previousQueriesComplex.put(sparql, gson.toJson(results));
         }
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        return gson.toJson(results);
+        return previousQueriesComplex.get(sparql);
     }
 
 
